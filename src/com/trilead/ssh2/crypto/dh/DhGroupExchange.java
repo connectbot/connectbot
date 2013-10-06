2f4da82b29f41a2fbc9669ff4852a8160bb7a6a7
Kenny Root <kenny@the-b.org>
2013-10-05 22:28:37 -0700

    Add diffie-hellman-group-exchange-sha256 support
    Support exchanging groups using SHA-256 as specified in RFC 4419
    
    For more information, see https://tools.ietf.org/html/rfc4419
    
    Change-Id: Iee5d29e7113a05cad4714a61321bf86b016624b8

--- src/com/trilead/ssh2/crypto/dh/DhGroupExchange.java
+++ src/com/trilead/ssh2/crypto/dh/DhGroupExchange.java
@@ -87,10 +87,10 @@ public class DhGroupExchange
 		this.k = f.modPow(x, p);
 	}
 
-	public byte[] calculateH(byte[] clientversion, byte[] serverversion, byte[] clientKexPayload,
-			byte[] serverKexPayload, byte[] hostKey, DHGexParameters para)
+	public byte[] calculateH(String hashAlgo, byte[] clientversion, byte[] serverversion,
+			byte[] clientKexPayload, byte[] serverKexPayload, byte[] hostKey, DHGexParameters para)
 	{
-		HashForSSH2Types hash = new HashForSSH2Types("SHA1");
+		HashForSSH2Types hash = new HashForSSH2Types(hashAlgo);
 
 		hash.updateByteString(clientversion);
 		hash.updateByteString(serverversion);
