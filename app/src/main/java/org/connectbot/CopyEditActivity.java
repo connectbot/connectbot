a588e39699ebac44e77c7a87e0e301ea690835f4
adb2001 <vacances2712>
2015-08-10 16:58:03 +0200

    Code style reformat.

--- app/src/main/java/org/connectbot/CopyEditActivity.java
+++ app/src/main/java/org/connectbot/CopyEditActivity.java
@@ -8,42 +8,37 @@ import android.widget.EditText;
 /**
  * Created by adb on 10/08/2015.
  */
-public class CopyEditActivity extends Activity
-{
-  public final static String TAG = CopyEditActivity.class.getSimpleName();
-  public static final String ARG_BUFFER = "buffer";
-  private EditText mEditText;
+public class CopyEditActivity extends Activity {
+    public final static String TAG = CopyEditActivity.class.getSimpleName();
+    public static final String ARG_BUFFER = "buffer";
+    private EditText mEditText;
 
-  @Override
-  protected void onCreate( Bundle savedInstanceState )
-  {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.act_copyedit);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_copyedit);
 
-    ActionBarWrapper actionBar = ActionBarWrapper.getActionBar(this);
-    actionBar.setDisplayHomeAsUpEnabled(true);
+        ActionBarWrapper actionBar = ActionBarWrapper.getActionBar(this);
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
-    Bundle bundle = getIntent().getExtras();
-    String buffer = bundle.getString(ARG_BUFFER);
+        Bundle bundle = getIntent().getExtras();
+        String buffer = bundle.getString(ARG_BUFFER);
 
-    if(buffer != null)
-    {
-      mEditText = (EditText) findViewById(R.id.editText);
-      mEditText.setText(buffer);
-      mEditText.setSelection(buffer.length());
+        if (buffer != null) {
+            mEditText = (EditText) findViewById(R.id.editText);
+            mEditText.setText(buffer);
+            mEditText.setSelection(buffer.length());
+        }
     }
-  }
 
-  @Override
-  public boolean onOptionsItemSelected( MenuItem item )
-  {
-    switch(item.getItemId())
-    {
-      case android.R.id.home:
-        finish();
-        return true;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
-    return super.onOptionsItemSelected(item);
-  }
 
 }
