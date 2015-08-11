4869b14b647a604d7416399663a382cf213e0fd2
adb2001 <vacances2712>
2015-08-11 09:48:32 +0200

    CodeStyle (Android Studio messes everything all the time...)

--- app/src/main/java/org/connectbot/CopyEditActivity.java
+++ app/src/main/java/org/connectbot/CopyEditActivity.java
@@ -1,3 +1,19 @@
+/*
+ * ConnectBot: simple, powerful, open-source SSH client for Android
+ * Copyright 2007 Kenny Root, Jeffrey Sharkey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.connectbot;
 
 import android.app.Activity;
@@ -5,40 +21,36 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.EditText;
 
-/**
- * Created by adb on 10/08/2015.
- */
 public class CopyEditActivity extends Activity {
-    public final static String TAG = CopyEditActivity.class.getSimpleName();
-    public static final String ARG_BUFFER = "buffer";
-    private EditText mEditText;
+	public final static String TAG = CopyEditActivity.class.getSimpleName();
+	public static final String ARG_BUFFER = "buffer";
+	private EditText mEditText;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.act_copyedit);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.act_copyedit);
 
-        ActionBarWrapper actionBar = ActionBarWrapper.getActionBar(this);
-        actionBar.setDisplayHomeAsUpEnabled(true);
+		ActionBarWrapper actionBar = ActionBarWrapper.getActionBar(this);
+		actionBar.setDisplayHomeAsUpEnabled(true);
 
-        Bundle bundle = getIntent().getExtras();
-        String buffer = bundle.getString(ARG_BUFFER);
+		Bundle bundle = getIntent().getExtras();
+		String buffer = bundle.getString(ARG_BUFFER);
 
-        if (buffer != null) {
-            mEditText = (EditText) findViewById(R.id.editText);
-            mEditText.setText(buffer);
-            mEditText.setSelection(buffer.length());
-        }
-    }
+		if (buffer != null) {
+			mEditText = (EditText) findViewById(R.id.editText);
+			mEditText.setText(buffer);
+			mEditText.setSelection(buffer.length());
+		}
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				finish();
+				return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
