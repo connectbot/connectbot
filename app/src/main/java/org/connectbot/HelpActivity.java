b1a9b3c724ac42fe667ca5523eb70490b4deaa54
Jeremy Klein <jlklein@google.com>
2015-08-20 13:44:12 -0700

    Fix checkstyle

--- app/src/main/java/org/connectbot/HelpActivity.java
+++ app/src/main/java/org/connectbot/HelpActivity.java
@@ -30,7 +30,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.TableRow;
 
 /**
  * @author Kenny Root
