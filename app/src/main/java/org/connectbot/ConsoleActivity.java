be9d9ac6ad2700361a44dc888d660a9e69e12833
Kenny Root <kenny@the-b.org>
2016-06-05 00:05:55 -0700

    Remove useless side-effect

--- app/src/main/java/org/connectbot/ConsoleActivity.java
+++ app/src/main/java/org/connectbot/ConsoleActivity.java
@@ -1037,7 +1037,7 @@ public class ConsoleActivity extends AppCompatActivity implements BridgeDisconne
 				try {
 					Log.d(TAG, String.format("We couldnt find an existing bridge with URI=%s (nickname=%s)," +
 							"so creating one now", requested.toString(), requested.getFragment()));
-					requestedBridge = bound.openConnection(requested);
+					bound.openConnection(requested);
 				} catch (Exception e) {
 					Log.e(TAG, "Problem while trying to create new requested bridge from URI", e);
 					// TODO: We should display an error dialog here.
