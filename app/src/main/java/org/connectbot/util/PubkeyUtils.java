353bf930a3f109507bcff156354db620a4b7f8f8
Kenny Root <kenny@the-b.org>
2016-06-11 21:01:30 -0700

    Make encodeHex package local

--- app/src/main/java/org/connectbot/util/PubkeyUtils.java
+++ app/src/main/java/org/connectbot/util/PubkeyUtils.java
@@ -353,7 +353,7 @@ public class PubkeyUtils {
 
 	private static final char[] HEX_DIGITS = { '0', '1', '2', '3', '4', '5', '6',
 			'7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
-	protected static String encodeHex(byte[] bytes) {
+	static String encodeHex(byte[] bytes) {
 		final char[] hex = new char[bytes.length * 2];
 
 		int i = 0;
