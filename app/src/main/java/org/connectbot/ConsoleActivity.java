6d18534a2ae6627746fa68300048d6397022a43e
Marc K <morckx@gmail.com>
2017-06-28 10:53:14 +0200

    Use "apply" to save edited preferences

--- app/src/main/java/org/connectbot/ConsoleActivity.java
+++ app/src/main/java/org/connectbot/ConsoleActivity.java
@@ -1551,7 +1551,7 @@ public class ConsoleActivity extends AppCompatActivity implements BridgeDisconne
 		}
 		String emulatedKeyMap = sb.toString();
 		edit.putString(PreferenceConstants.KEY_MAP, emulatedKeyMap);
-		edit.commit();
+		edit.apply();
 	}
 
 	public int[] loadEmulatedKeyMap() {
