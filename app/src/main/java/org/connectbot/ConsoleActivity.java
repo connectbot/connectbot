connectbot/connectbot

commit 36b4b5ea92bc3ede9626d73503d9139f97044cc4

Various style fixes

app/src/main/java/org/connectbot/ConsoleActivity.java
@@ -124,7 +124,7 @@ public class ConsoleActivity extends AppCompatActivity implements BridgeDisconne
 
 	private RelativeLayout booleanPromptGroup;
 	private TextView booleanPrompt;
-	private Button booleanYes, booleanNo;
+	private Button booleanYes;
 
 	private LinearLayout keyboardGroup;
 	private Runnable keyboardGroupHider;
@@ -134,8 +134,6 @@ public class ConsoleActivity extends AppCompatActivity implements BridgeDisconne
 	private Animation fade_out_delayed;
 
 	private Animation keyboard_fade_in, keyboard_fade_out;
-
-	private InputMethodManager inputManager;
 
 	private MenuItem disconnect, copy, paste, portForward, resize, urlscan;
 
@@ -378,10 +376,11 @@ public class ConsoleActivity extends AppCompatActivity implements BridgeDisconne
 			break;
 		}
 
-		if (hideKeys)
+		if (hideKeys) {
 			hideEmulatedKeys();
-		else
+		} else {
 			autoHideEmulatedKeys();
+		}
 
 		terminal.bridge.tryKeyVibrate();
 		hideActionBarIfRequested();
@@ -437,14 +436,17 @@ public class ConsoleActivity extends AppCompatActivity implements BridgeDisconne
 	}
 
 	private void autoHideEmulatedKeys() {
-		if (keyboardGroupHider != null)
+		if (keyboardGroupHider != null) {
 			handler.removeCallbacks(keyboardGroupHider);
+		}
+
 		keyboardGroupHider = new Runnable() {
 			public void run() {
-				if (keyboardGroup.getVisibility() == View.GONE || inActionBarMenu)
+				if (keyboardGroup.getVisibility() == View.GONE || inActionBarMenu) {
 					return;
+				}
 
-				if (keyboardAlwaysVisible == false) {
+				if (!keyboardAlwaysVisible) {
 					keyboardGroup.startAnimation(keyboard_fade_out);
 					keyboardGroup.setVisibility(View.GONE);
 				}
@@ -456,7 +458,7 @@ public class ConsoleActivity extends AppCompatActivity implements BridgeDisconne
 	}
 
 	private void hideEmulatedKeys() {
-		if (keyboardAlwaysVisible == false) {
+		if (!keyboardAlwaysVisible) {
 			if (keyboardGroupHider != null)
 				handler.removeCallbacks(keyboardGroupHider);
 			keyboardGroup.setVisibility(View.GONE);
@@ -565,7 +567,7 @@ public class ConsoleActivity extends AppCompatActivity implements BridgeDisconne
 			}
 		});
 
-		booleanNo = (Button) findViewById(R.id.console_prompt_no);
+		Button booleanNo = (Button) findViewById(R.id.console_prompt_no);
 		booleanNo.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				PromptHelper helper = getCurrentPromptHelper();
@@ -580,8 +582,6 @@ public class ConsoleActivity extends AppCompatActivity implements BridgeDisconne
 		// Preload animation for keyboard button
 		keyboard_fade_in = AnimationUtils.loadAnimation(this, R.anim.keyboard_fade_in);
 		keyboard_fade_out = AnimationUtils.loadAnimation(this, R.anim.keyboard_fade_out);
-
-		inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 
 		keyboardGroup = (LinearLayout) findViewById(R.id.keyboard_group);
 
@@ -650,7 +650,7 @@ public class ConsoleActivity extends AppCompatActivity implements BridgeDisconne
 			actionBar.addOnMenuVisibilityListener(new ActionBar.OnMenuVisibilityListener() {
 				public void onMenuVisibilityChanged(boolean isVisible) {
 					inActionBarMenu = isVisible;
-					if (isVisible == false) {
+					if (!isVisible) {
 						hideEmulatedKeys();
 					}
 				}
@@ -1185,7 +1185,7 @@ public class ConsoleActivity extends AppCompatActivity implements BridgeDisconne
 				TextView urlView = (TextView) view;
 
 				String url = urlView.getText().toString();
-				if (url.indexOf("://") < 0)
+				if (url.contains("://"))
 					url = "http://" + url;
 
 				Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));

app/src/main/java/org/connectbot/PortForwardListActivity.java
@@ -26,6 +26,7 @@ import org.connectbot.service.TerminalBridge;
 import org.connectbot.service.TerminalManager;
 import org.connectbot.util.HostDatabase;
 
+import android.annotation.SuppressLint;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -267,6 +268,7 @@ public class PortForwardListActivity extends AppCompatListActivity {
 			}
 		}
 
+		@SuppressLint("DefaultLocale")
 		@Override
 		public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
 			// Create menu to handle deleting and editing port forward
@@ -404,8 +406,7 @@ public class PortForwardListActivity extends AppCompatListActivity {
 		public PortForwardViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 			View v = LayoutInflater.from(parent.getContext())
 					.inflate(R.layout.item_portforward, parent, false);
-			PortForwardViewHolder vh = new PortForwardViewHolder(v);
-			return vh;
+			return new PortForwardViewHolder(v);
 		}
 
 		@Override

app/src/main/java/org/connectbot/bean/HostBean.java
@@ -23,6 +23,7 @@ import org.connectbot.transport.Telnet;
 import org.connectbot.transport.TransportFactory;
 import org.connectbot.util.HostDatabase;
 
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.net.Uri;
 
@@ -197,6 +198,7 @@ public class HostBean extends AbstractBean {
 		return quickDisconnect;
 	}
 
+	@SuppressLint("DefaultLocale")
 	public String getDescription() {
 		String description = String.format("%s@%s", username, hostname);
 

app/src/main/java/org/connectbot/bean/PortForwardBean.java
@@ -19,6 +19,7 @@ package org.connectbot.bean;
 
 import org.connectbot.util.HostDatabase;
 
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
 
 
@@ -203,6 +204,7 @@ public class PortForwardBean extends AbstractBean {
 	/**
 	 * @return human readable description of the port forward
 	 */
+	@SuppressLint("DefaultLocale")
 	public CharSequence getDescription() {
 		String description = "Unknown type";
 
@@ -210,10 +212,6 @@ public class PortForwardBean extends AbstractBean {
 			description = String.format("Local port %d to %s:%d", sourcePort, destAddr, destPort);
 		} else if (HostDatabase.PORTFORWARD_REMOTE.equals(type)) {
 			description = String.format("Remote port %d to %s:%d", sourcePort, destAddr, destPort);
-/* I don't think we need the SOCKS4 type.
-		} else if (HostDatabase.PORTFORWARD_DYNAMIC4.equals(type)) {
-			description = String.format("Dynamic port %d (SOCKS4)", sourcePort);
-*/
 		} else if (HostDatabase.PORTFORWARD_DYNAMIC5.equals(type)) {
 			description = String.format("Dynamic port %d (SOCKS)", sourcePort);
 		}

app/src/main/java/org/connectbot/service/TerminalBridge.java
@@ -61,7 +61,7 @@ import de.mud.terminal.vt320;
 public class TerminalBridge implements VDUDisplay {
 	public final static String TAG = "CB.TerminalBridge";
 
-	public final static int DEFAULT_FONT_SIZE_DP = 10;
+	private final static int DEFAULT_FONT_SIZE_DP = 10;
 	private final static int FONT_SIZE_STEP = 2;
 	private final float displayDensity;
 
@@ -96,13 +96,10 @@ public class TerminalBridge implements VDUDisplay {
 	private int columns;
 	private int rows;
 
-	/* package */ final TerminalKeyListener keyListener;
+	private final TerminalKeyListener keyListener;
 
 	private boolean selectingForCopy = false;
 	private final SelectionArea selectionArea;
-
-	// TODO add support for the new clipboard API
-	private ClipboardManager clipboard;
 
 	public int charWidth = -1;
 	public int charHeight = -1;
@@ -122,7 +119,7 @@ public class TerminalBridge implements VDUDisplay {
 
 	public PromptHelper promptHelper;
 
-	protected BridgeDisconnectedListener disconnectListener = null;
+	private BridgeDisconnectedListener disconnectListener = null;
 
 	/**
 	 * Create a new terminal bridge suitable for unit testing.
@@ -157,7 +154,7 @@ public class TerminalBridge implements VDUDisplay {
 
 		transport = null;
 
-		keyListener = new TerminalKeyListener(manager, this, buffer, null);
+		keyListener = new TerminalKeyListener(null, this, buffer, null);
 	}
 
 	/**
@@ -263,6 +260,11 @@ public class TerminalBridge implements VDUDisplay {
 	 */
 	protected void startConnection() {
 		transport = TransportFactory.getTransport(host.getProtocol());
+		if (transport == null) {
+			Log.i(TAG, "No transport found for " + host.getProtocol());
+			return;
+		}
+
 		transport.setBridge(this);
 		transport.setManager(manager);
 		transport.setHost(host);
@@ -287,18 +289,6 @@ public class TerminalBridge implements VDUDisplay {
 		connectionThread.setName("Connection");
 		connectionThread.setDaemon(true);
 		connectionThread.start();
-	}
-
-	/**
-	 * Handle challenges from keyboard-interactive authentication mode.
-	 */
-	public String[] replyToChallenge(String name, String instruction, int numPrompts, String[] prompt, boolean[] echo) {
-		String[] responses = new String[numPrompts];
-		for (int i = 0; i < numPrompts; i++) {
-			// request response from user for each prompt
-			responses[i] = promptHelper.requestStringPrompt(instruction, prompt[i]);
-		}
-		return responses;
 	}
 
 	/**
@@ -607,7 +597,7 @@ public class TerminalBridge implements VDUDisplay {
 		if (width <= 0 || height <= 0)
 			return;
 
-		clipboard = (ClipboardManager) parent.getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+		ClipboardManager clipboard = (ClipboardManager) parent.getContext().getSystemService(Context.CLIPBOARD_SERVICE);
 		keyListener.setClipboardManager(clipboard);
 
 		if (!forcedSize) {

app/src/main/java/org/connectbot/transport/SSH.java
@@ -111,10 +111,8 @@ public class SSH extends AbsTransport implements ConnectionMonitor, InteractiveC
 
 	private final static int AUTH_TRIES = 20;
 
-	static final Pattern hostmask;
-	static {
-		hostmask = Pattern.compile("^(.+)@(([0-9a-z.-]+)|(\\[[a-f:0-9]+\\]))(:(\\d+))?$", Pattern.CASE_INSENSITIVE);
-	}
+	private static final Pattern hostmask = Pattern.compile(
+			"^(.+)@(([0-9a-z.-]+)|(\\[[a-f:0-9]+\\]))(:(\\d+))?$", Pattern.CASE_INSENSITIVE);
 
 	private boolean compression = false;
 	private volatile boolean authenticated = false;
@@ -126,7 +124,6 @@ public class SSH extends AbsTransport implements ConnectionMonitor, InteractiveC
 
 	private Connection connection;
 	private Session session;
-	private ConnectionInfo connectionInfo;
 
 	private OutputStream stdin;
 	private InputStream stdout;
@@ -183,12 +180,14 @@ public class SSH extends AbsTransport implements ConnectionMonitor, InteractiveC
 				bridge.outputLine(manager.res.getString(R.string.host_fingerprint, algorithmName, fingerprint));
 
 				result = bridge.promptHelper.requestBooleanPrompt(null, manager.res.getString(R.string.prompt_continue_connecting));
-				if (result == null) return false;
-				if (result.booleanValue()) {
+				if (result == null) {
+					return false;
+				}
+				if (result) {
 					// save this key in known database
 					manager.hostdb.saveKnownHost(hostname, port, serverHostKeyAlgorithm, serverHostKey);
 				}
-				return result.booleanValue();
+				return result;
 
 			case KnownHosts.HOSTKEY_HAS_CHANGED:
 				String header = String.format("@   %s   @",
@@ -209,7 +208,7 @@ public class SSH extends AbsTransport implements ConnectionMonitor, InteractiveC
 
 				// Users have no way to delete keys, so we'll prompt them for now.
 				result = bridge.promptHelper.requestBooleanPrompt(null, manager.res.getString(R.string.prompt_continue_connecting));
-				if (result != null && result.booleanValue()) {
+				if (result != null && result) {
 					// save this key in known database
 					manager.hostdb.saveKnownHost(hostname, port, serverHostKeyAlgorithm, serverHostKey);
 					return true;
@@ -460,7 +459,7 @@ public class SSH extends AbsTransport implements ConnectionMonitor, InteractiveC
 			Logger.enabled = true;
 			Logger.logger = logger;
 			*/
-			connectionInfo = connection.connect(new HostKeyVerifier());
+			ConnectionInfo connectionInfo = connection.connect(new HostKeyVerifier());
 			connected = true;
 
 			bridge.outputLine(manager.res.getString(R.string.terminal_kex_algorithm,

app/src/main/java/org/connectbot/transport/Telnet.java
@@ -37,6 +37,7 @@ import org.connectbot.service.TerminalBridge;
 import org.connectbot.service.TerminalManager;
 import org.connectbot.util.HostDatabase;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.net.Uri;
 import android.util.Log;
@@ -248,6 +249,7 @@ public class Telnet extends AbsTransport {
 		}
 	}
 
+	@SuppressLint("DefaultLocale")
 	@Override
 	public String getDefaultNickname(String username, String hostname, int port) {
 		if (port == DEFAULT_PORT) {

app/src/main/java/org/connectbot/util/HostDatabase.java
@@ -23,6 +23,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -657,7 +658,8 @@ public class HostDatabase extends RobustSQLiteOpenHelper implements HostStorage,
 				if (hostkey == null || hostkey.length == 0) continue;
 
 				try {
-					known.addHostkey(new String[] {String.format("%s:%d", hostname, port)}, hostkeyalgo, hostkey);
+					known.addHostkey(new String[] {String.format(Locale.US, "%s:%d", hostname, port)},
+							hostkeyalgo, hostkey);
 				} catch (Exception e) {
 					Log.e(TAG, "Problem while adding a known host from database", e);
 				}

commit 3e89680c08ddb059e8ac05bea04d6e7733ac2a41

do not prefix urls with http://
this was broken by inverted logic in
36b4b5ea92bc3ede9626d73503d9139f97044cc4 -- http:// was being prepended
to urls which already contained it. But since URL scan doesn't seem to
pick up URLs without scheme anyway, this check is redundant.

app/src/main/java/org/connectbot/ConsoleActivity.java
@@ -1185,9 +1185,6 @@ public class ConsoleActivity extends AppCompatActivity implements BridgeDisconne
 				TextView urlView = (TextView) view;
 
 				String url = urlView.getText().toString();
-				if (url.contains("://"))
-					url = "http://" + url;
-
 				Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
 				context.startActivity(intent);
 			} catch (Exception e) {