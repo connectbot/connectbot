338715a3e16fcc1afef10be323477ab5693d7507
Kenny Root <kenny@the-b.org>
2018-05-24 17:42:36 +0900

    No need to have final on private

--- app/src/main/java/org/connectbot/service/TerminalBridge.java
+++ app/src/main/java/org/connectbot/service/TerminalBridge.java
@@ -523,7 +523,7 @@ public class TerminalBridge implements VDUDisplay {
 	 *
 	 * @param sizeDp Size of font in dp
 	 */
-	private final void setFontSize(float sizeDp) {
+	private void setFontSize(float sizeDp) {
 		if (sizeDp <= 0.0) {
 			return;
 		}
