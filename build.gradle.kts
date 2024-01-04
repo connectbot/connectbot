be92f2650588a6e689b8c6ee06ef82bd94aa1a50
Kenny Root <kenny@the-b.org>
2024-01-03 22:12:50 -0600

    Convert build.gradle to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+	repositories {
+		google()
+		mavenCentral()
+	}
+}
+
+plugins {
+	id("com.diffplug.spotless") version "6.23.3" apply false
+}
+
+val translationsOnly: String = System.getenv("TRANSLATIONS_ONLY")?.trim() ?: ""
+
+allprojects {
+	buildscript {
+		repositories {
+			google()
+			mavenCentral()
+		}
+	}
+	repositories {
+		google()
+		mavenCentral()
+	}
+
+	if (translationsOnly.isEmpty()) {
+		apply {
+			plugin("com.diffplug.spotless")
+		}
+	}
+}
+
+if (translationsOnly.isEmpty()) {
+	apply {
+		plugin("com.diffplug.spotless")
+	}
+
+	configure<com.diffplug.gradle.spotless.SpotlessExtension>  {
+		groovyGradle {
+			target("**/*.gradle")
+		}
+
+		format("misc") {
+			target("**/*.md", "**/.gitignore")
+
+			trimTrailingWhitespace()
+			endWithNewline()
+		}
+	}
+}
