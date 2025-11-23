d072ea8208d1cf4f6d04ed38a106e8a675673f62
Kenny Root <kenny@the-b.org>
2025-11-22 22:30:12 -0800

    Fix screens to allow scroll with IME
    When editing a text field with edge-to-edge rendering, you need to
    account for the IME insets to avoid having to dismiss the IME to click
    on something near the end of the Column.

--- app/src/main/java/org/connectbot/ui/screens/hosteditor/HostEditorScreen.kt
+++ app/src/main/java/org/connectbot/ui/screens/hosteditor/HostEditorScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -172,6 +173,7 @@ fun HostEditorScreenContent(
                 .fillMaxSize()
                 .verticalScroll(rememberScrollState())
                 .padding(16.dp)
+                .imePadding()
         ) {
             if (!expandedMode) {
                 // Quick connect mode
