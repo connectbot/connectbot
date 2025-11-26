f785803cddac08bd32c4b3fde863d21eb5a69277
Kenny Root <kenny@the-b.org>
2025-11-26 14:47:57 -0800

    Unify linting under spotless
    We didn't have a Kotlin lint before, so add that to make sure the code
    stays consistent.

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,47 @@ allprojects {
     }
 }
 
-if (!extra.has("TRANSLATIONS_ONLY") || extra.get("TRANSLATIONS_ONLY") == null) {
-    apply(from = "spotless.gradle")
+spotless {
+    ratchetFrom = "origin/main"
+
+    java {
+        target("app/src/main/java/org/connectbot/**/*.java")
+        importOrder()
+        removeUnusedImports()
+        googleJavaFormat()
+    }
+
+    kotlin {
+        target("app/src/**/*.kt")
+        ktlint("1.8.0")
+            .editorConfigOverride(
+                mapOf(
+                    "ij_kotlin_imports_layout" to "*,java.**,javax.**,kotlin.**,^",
+                    "ktlint_code_style" to "android_studio",
+                    "ktlint_function_naming_ignore_when_annotated_with" to "Composable",
+                    "ktlint_standard_property-naming" to "disabled",
+                    "ktlint_standard_backing-property-naming" to "disabled",
+                    "ktlint_standard_filename" to "disabled",
+                    "ktlint_standard_discouraged-comment-location" to "disabled",
+                    "ktlint_standard_max-line-length" to "disabled",
+                    "ktlint_standard_kdoc" to "disabled",
+                ),
+            ).customRuleSets(listOf("io.nlopez.compose.rules:ktlint:0.4.28"))
+    }
+
+    groovyGradle {
+        target("**/*.gradle")
+    }
+
+    kotlinGradle {
+        target("**/*.gradle.kts")
+        ktlint()
+    }
+
+    format("misc") {
+        target(listOf("**/*.md", "**/.gitignore"))
+
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,7 @@ pluginManagement {
     }
 }
 
+@file:Suppress("ktlint:standard:property-naming")
 val TRANSLATIONS_ONLY: String? by settings
 
 if (TRANSLATIONS_ONLY.isNullOrBlank()) {
