5d170b2939782909521a292a7ad3a478915cc648
Kenny Root <kenny@the-b.org>
2026-01-23 20:45:41 -0800

    chore(build): migrate to built-in Kotlin
    AGP 9.0 and newer has Kotlin built-in so we no longer need the plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,20 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
+    val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
     extra.set("TRANSLATIONS_ONLY", System.getenv("TRANSLATIONS_ONLY")?.trim())
     repositories {
         google()
         mavenCentral()
     }
+    dependencies {
+        classpath(libs.findLibrary("kotlin-gradle-plugin").get())
+        classpath(libs.findLibrary("ksp-gradle-plugin").get())
+    }
 }
 
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.spotless)
