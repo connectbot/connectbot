1056254b076610b70d112eacba00615cbc069b41
Kenny Root <kenny@the-b.org>
2026-02-12 21:23:07 +0800

    Fix font downloading dialog layout

--- app/src/main/java/org/connectbot/ui/components/FontDownloadProgressDialog.kt
+++ app/src/main/java/org/connectbot/ui/components/FontDownloadProgressDialog.kt
@@ -19,32 +19,49 @@ package org.connectbot.ui.components
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.connectbot.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FontDownloadProgressDialog() {
-    AlertDialog(
+fun FontDownloadProgressDialog(
+    modifier: Modifier = Modifier
+) {
+    BasicAlertDialog(
         onDismissRequest = {},
-        confirmButton = {},
-        text = {
+        modifier = modifier
+    ) {
+        Surface(
+            shape = MaterialTheme.shapes.extraLarge,
+            tonalElevation = 6.dp
+        ) {
             Row(
                 verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.padding(24.dp)
             ) {
                 CircularProgressIndicator()
                 Spacer(modifier = Modifier.width(16.dp))
                 Text(stringResource(R.string.font_downloading))
             }
         }
-    )
+    }
+}
+
+@Preview
+@Composable
+private fun FontDownloadProgressDialogPreview() {
+    FontDownloadProgressDialog()
 }
