9832dc22da8d38dd5b6c1a37fcaf16d45f68ddbf
nindanaoto <matsuoka.kotaro@gmail.com>
2026-04-24 03:02:42 +0000

    Apply Spotless after rebase

--- app/src/main/java/org/connectbot/ui/screens/console/ConsoleScreen.kt
+++ app/src/main/java/org/connectbot/ui/screens/console/ConsoleScreen.kt
@@ -1188,7 +1188,7 @@ fun ConsoleScreen(
                 onConfirm = {
                     showDisconnectDialog = false
                     viewModel.disconnectCurrentSession()
-                }
+                },
             )
         }
 
