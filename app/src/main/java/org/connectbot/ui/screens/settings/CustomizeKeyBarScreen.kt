0cceeb54185ed6a4f8ca798b8c225f4852968f82
aoleg94 <aoleg94@gmail.com>
2026-05-12 21:18:28 +0500

    Align hiltViewModel import with project convention

--- app/src/main/java/org/connectbot/ui/screens/settings/CustomizeKeyBarScreen.kt
+++ app/src/main/java/org/connectbot/ui/screens/settings/CustomizeKeyBarScreen.kt
@@ -57,7 +57,7 @@ import androidx.compose.ui.semantics.CustomAccessibilityAction
 import androidx.compose.ui.semantics.customActions
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import org.connectbot.R
 import org.connectbot.util.keybar.BuiltinKeyId
 import org.connectbot.util.keybar.KeyEntry
