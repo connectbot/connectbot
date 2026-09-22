aa05ecaf1ee4b704c0ce8dc0fd55079d6d570da1
Kenny Root <kenny@the-b.org>
2026-09-21 22:12:16 -0700

    feat: add InlineImages profile setting
    termlib now supports Kitty and iTerm2 style inline images. Add a
    preference in Profiles (defaulting to Ask) for users to enjoy this new
    feature.

---
+++ app/src/test/kotlin/org/connectbot/data/migration/InlineImagesMigrationTest.kt
@@ -0,0 +1,66 @@
+/*
+ * ConnectBot: simple, powerful, open-source SSH client for Android
+ * Copyright 2026 Kenny Root
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.connectbot.data.migration
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import kotlinx.coroutines.test.runTest
+import org.connectbot.data.ConnectBotDatabase
+import org.json.JSONObject
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import java.io.File
+
+@RunWith(RobolectricTestRunner::class)
+class InlineImagesMigrationTest {
+    @Test
+    fun existingProfilesDefaultToAskAfterUpgrade() = runTest {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val name = "inline-images-migration"
+        context.deleteDatabase(name)
+        val schema = JSONObject(File("schemas/org.connectbot.data.ConnectBotDatabase/8.json").readText())
+            .getJSONObject("database")
+        context.openOrCreateDatabase(name, Context.MODE_PRIVATE, null).use { db ->
+            val entities = schema.getJSONArray("entities")
+            for (i in 0 until entities.length()) {
+                val entity = entities.getJSONObject(i)
+                val table = entity.getString("tableName")
+                db.execSQL(entity.getString("createSql").replace("\${TABLE_NAME}", table))
+                val indices = entity.getJSONArray("indices")
+                for (j in 0 until indices.length()) {
+                    db.execSQL(indices.getJSONObject(j).getString("createSql").replace("\${TABLE_NAME}", table))
+                }
+            }
+            db.execSQL("INSERT INTO profiles (name) VALUES ('Existing')")
+            db.version = 8
+        }
+        val database = Room.databaseBuilder(context, ConnectBotDatabase::class.java, name).build()
+        try {
+            val profile = database.profileDao().getAll().single()
+            assertEquals("ask", profile.inlineImages)
+            database.profileDao().update(profile.copy(inlineImages = "on"))
+            assertEquals("on", database.profileDao().getAll().single().inlineImages)
+        } finally {
+            database.close()
+            context.deleteDatabase(name)
+        }
+    }
+}
